fa29cffb8071e54043062351aab84c3645b98292
Junichiro Takagi <t.junichiro@gmail.com>
2019-09-28 20:40:51 +0900

    create api with kubebuilder

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	spannerv1 "github.com/tjun/spanner-autoscaler/api/v1"
+	"github.com/tjun/spanner-autoscaler/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = spannerv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -59,6 +62,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.SpannerAutoscalerReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SpannerAutoscaler"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SpannerAutoscaler")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
