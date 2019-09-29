fa32d0944cf404a100b4f1c67aa5e9f6db334faf
Junichiro Takagi <t.junichiro@gmail.com>
2019-09-29 14:57:48 +0900

    add spec and status for spanner autoscaler

--- api/v1beta1/spannerautoscaler_types.go
+++ api/v1beta1/spannerautoscaler_types.go
@@ -22,16 +22,51 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// CPUTargetUtilization defines the utilization of Cloud Spanner CPU
+type CPUTargetUtilization struct {
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=100
+	// fraction of the requested CPU that should be utilized/used,
+	// e.g. 70 means that 70% of the requested CPU should be in use.
+	TargetPercentage int `json:"targetPercentage"`
+}
+
 // SpannerAutoscalerSpec defines the desired state of SpannerAutoscaler
 type SpannerAutoscalerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// +kubebuilder:validation:Minimum=1
+	// lower limit for the number of nodes that can be set by the autoscaler, default 1.
+	MinNodes *int `json:"minNodes"`
+
+	// +kubebuilder:validation:Minimum=1
+	// upper limit for the number of nodes that can be set by the autoscaler.
+	// It cannot be smaller than MinNodes.
+	MaxNodes int `json:"maxNodes"`
+
+	// target average CPU utilization for Spanner with priority = high.
+	HighPriorityCPUUtilization CPUTargetUtilization `json:"highPriorityCPUUtilization"`
 }
 
 // SpannerAutoscalerStatus defines the observed state of SpannerAutoscaler
 type SpannerAutoscalerStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// last time the SpannerAutoscaler scaled the number of Spanner nodes.
+	// used by the autoscaler to controle how often the number of nodes is changed.
+	// +optional
+	LastScaleTime *metav1.Time `json:"lastScaleTime,omitempty"`
+
+	// current number of nodes of Spanner managed by this autoscaler.
+	CurrentNodes int `json:"currentNodes,omitempty"`
+
+	// desired number of nodes of Spanner managed by this autoscaler.
+	DesiredNodes int `json:"desiredNodes,omitempty"`
+
+	// current average CPU utilization for high proority task, represented as a percentage.
+	CurrentHighPriorityCPUUtilizationPercentage *int `json:"currentHighPriorityCPUUtilizationPercentage,omitempty"`
 }
 
 // +kubebuilder:object:root=true
